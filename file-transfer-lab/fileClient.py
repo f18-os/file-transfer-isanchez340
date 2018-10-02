3805b8c215af6b2293e7cf7a5f7260dee13199c7
irving <sanchez.irving006@gmail.com>
2018-10-01 20:55:55 -0600

    more client side code

--- file-transfer-lab/fileClient.py
+++ file-transfer-lab/fileClient.py
@@ -11,3 +11,39 @@ switchesVarDefaults = (
 
 progname = "fileClient"
 paramMap = params.parseParams(switchesVarDefaults)
+
+server, usage  = paramMap["server"], paramMap["usage"]
+
+if usage:
+    params.usage()
+
+try:
+    serverHost, serverPort = re.split(":", server)
+    serverPort = int(serverPort)
+except:
+    print("Can't parse server:port from '%s'" % server)
+    sys.exit(1)
+
+s = None
+for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
+    af, socktype, proto, canonname, sa = res
+    try:
+        print("creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto))
+        s = socket.socket(af, socktype, proto)
+    except socket.error as msg:
+        print(" error: %s" % msg)
+        s = None
+        continue
+    try:
+        print(" attempting to connect to %s" % repr(sa))
+        s.connect(sa)
+    except socket.error as msg:
+        print(" error: %s" % msg)
+        s.close()
+        s = None
+        continue
+    break
+
+if s is None:
+    print('could not open socket')
+    sys.exit(1)
--- file-transfer-lab/fileServer.py
+++ file-transfer-lab/fileServer.py
@@ -14,3 +14,15 @@ paramMap = params.parseParams(switchesVarDefaults)
 
 listenPort = paramMap['listenPort']
 listenAddr = ''       # Symbolic name meaning all available interfaces
+
+if paramMap['usage']:
+    params.usage()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((listenAddr, listenPort))
+s.listen(1)              # allow only one outstanding request
+# s is a factory for connected sockets
+
+conn, addr = s.accept()  # wait until incoming connection request (and accept it)
+print('Connected by', addr)
+
