1735f4dcdc974efc7dee9c68e79a65e1c3612baa
irving <sanchez.irving006@gmail.com>
2018-10-01 20:15:52 -0600

    basic server and client code, about to start transfer files

--- file-transfer-lab/fileClient.py
+++ file-transfer-lab/fileClient.py
@@ -3,3 +3,11 @@
 import sys, re, socket
 sys.path.append("../lib") #for params
 import params
+
+switchesVarDefaults = (
+    (('-l', '--listenPort') ,'listenPort', 50001),
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+progname = "fileClient"
+paramMap = params.parseParams(switchesVarDefaults)
--- file-transfer-lab/fileServer.py
+++ file-transfer-lab/fileServer.py
@@ -3,3 +3,14 @@
 import sys, re, socket
 sys.path.append("../lib") #for params
 import params
+
+switchesVarDefaults = (
+    (('-l', '--listenPort') ,'listenPort', 50001),
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+progname = "fileServer"
+paramMap = params.parseParams(switchesVarDefaults)
+
+listenPort = paramMap['listenPort']
+listenAddr = ''       # Symbolic name meaning all available interfaces
